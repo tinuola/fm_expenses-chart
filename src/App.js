f1ad5e0f8e032277ed74e09297215791c4a6c49a
tinuola <24995224+tinuola@users.noreply.github.com>
2023-05-25 23:14:37 -0700

    Implement state change based on day

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import React from 'react';
+import { useState } from 'react';
 import Cell from './Cell';
 import Data from './data.json';
 
 export function App() {
+  const today = new Date();
+  const nameDay = today.getDay();
+
+  const [day, setDay] = useState(nameDay);
+
+  console.log('loaded');
+
+  function checkDate(n) {
+    if (n === nameDay) {
+      return true;
+    }
+  }
+
   return (
     <div className='chart-container'>
       {Data.map((item) => {
         const { id, day, amount } = item;
+        let status = checkDate(id);
+        // console.log(status);
         return (
           <Cell
             key={id}
             day={day}
             amount={amount}
+            active={status}
           />
         );
       })}
